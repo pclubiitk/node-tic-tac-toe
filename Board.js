b4ad1e705188a93cb363c4bf39a15933ab2081d6
rabisg <guha.rabishankar@gmail.com>
2014-01-25 17:43:03 +0530

    Add gameplay logic

--- Board.js
+++ Board.js
@@ -9,7 +9,41 @@ board.onclick = function(event) {
 
   if(moves[row-1][col-1] == 0) {
     event.target.innerHTML = turn;
-    turn = (turn == 'X') ? 'O' : 'X';
     moves[row-1][col-1] = turn;
+    if(playerWon(row-1, col-1)) display("Player " + turn + " won");
+    else if(gameOver()==true) display("Nobody Won");
+    turn = (turn == 'X') ? 'O' : 'X';
   }
+}
+
+function gameOver() {
+  for(var i=0; i<3; i++)
+    for(var j=0; j<3; j++)
+      if(moves[i][j]==0)
+        return false;
+
+  return true;
+}
+
+function playerWon(i, j) {
+  var thisMove = moves[i][j];
+  //Check for row first
+  if (moves[i][0] == moves[i][1] && moves[i][1] == moves[i][2])
+    return true;
+  //Check for column now
+  if (moves[0][j] == moves[1][j] && moves[1][j] == moves[2][j])
+    return true;
+
+  //Check for leading diagonal
+  if (moves[0][0] == thisMove && moves[1][1] == thisMove && moves[2][2] == thisMove)
+    return true;
+  //Check for other diagonal
+  if (moves[0][2] == thisMove && moves[1][1] == thisMove && moves[2][0] == thisMove)
+    return true;
+
+  return false;
+}
+
+function display(str) {
+  document.getElementById('message').innerHTML = str;
 }
